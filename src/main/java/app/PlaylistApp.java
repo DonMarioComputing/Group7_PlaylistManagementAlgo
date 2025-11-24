180b7e2f42060f264b813cc664fe13fce74a6882
michelle <michelle.graham@dkit.ie>
2025-11-24 10:53:52 +0000

    feat: bad change

--- src/main/java/app/PlaylistApp.java
+++ src/main/java/app/PlaylistApp.java
@@ -7,5 +7,6 @@ public class PlaylistApp {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is a naive change!");
     }
 }
